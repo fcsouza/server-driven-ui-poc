b83c25f1ac9b927f7b997ef20d0770b4ec7f9254
fcsouza <fcsouza18@hotmail.com>
2020-12-02 21:27:01 -0300

    include navbar

--- bff/src/main/kotlin/com/example/bff/builder/OutfitScreen.kt
+++ bff/src/main/kotlin/com/example/bff/builder/OutfitScreen.kt
@@ -1,18 +1,44 @@
 package com.example.bff.builder
 
-import br.com.zup.beagle.widget.layout.Container
-import br.com.zup.beagle.widget.layout.Screen
-import br.com.zup.beagle.widget.layout.ScreenBuilder
+import br.com.zup.beagle.ui.image
+import br.com.zup.beagle.ui.text
+import br.com.zup.beagle.widget.action.Alert
+import br.com.zup.beagle.widget.layout.*
+import br.com.zup.beagle.widget.ui.ImagePath
 import br.com.zup.beagle.widget.ui.Text
 
 class OutfitScreen :ScreenBuilder {
     override fun build(): Screen {
         return Screen(
-                child = Container(
-                   children = listOf(
-                           Text(text= "Hello World", textColor = "#FFFFF")
-                   )
+            navigationBar = navBar(),
+            child = Text(text = "Hello World")
+        )
+    }
+
+    private fun navBar(): NavigationBar {
+        return NavigationBar(
+            title = "Outfit",
+            styleId = "customNavigation",
+            navigationBarItems = listOf(
+                NavigationBarItem(
+                    text = "Heart Icon",
+                    image = ImagePath.Local.justMobile("heart"),
+                    action = Alert(
+                        title = "Clicked Heart Icon",
+                        message = "You've clicked in Heart Icon",
+                        labelOk = "Ok"
+                    )
+                ),
+                NavigationBarItem(
+                    text = "Bag Icon",
+                    image = ImagePath.Local.justMobile("bag"),
+                    action = Alert(
+                        title = "Clicked Bag Icon",
+                        message = "You've clicked in Bag Icon",
+                        labelOk = "Ok"
+                    )
                 )
+            )
         )
     }
 }

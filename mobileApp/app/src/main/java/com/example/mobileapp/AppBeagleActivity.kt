0a04f533d357ce30e915fff6ec0c9567e0556191
fcsouza <fcsouza18@hotmail.com>
2020-12-11 22:03:28 -0300

    include beagle activity

--- mobileApp/app/src/main/java/com/example/mobileapp/AppBeagleActivity.kt
+++ mobileApp/app/src/main/java/com/example/mobileapp/AppBeagleActivity.kt
@@ -2,11 +2,40 @@ package com.example.mobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import br.com.zup.beagle.android.annotation.BeagleComponent
+import br.com.zup.beagle.android.view.BeagleActivity
+import br.com.zup.beagle.android.view.ServerDrivenState
 
-class AppBeagleActivity : AppCompatActivity() {
+@BeagleComponent
+class AppBeagleActivity : BeagleActivity() {
+
+    private val progressBar: ProgressBar by lazy { findViewById<ProgressBar>(R.id.progress_bar) }
+    private val mToolbar: Toolbar by lazy { findViewById<Toolbar>(R.id.custom_toolbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_beagle)
+    }
+
+    override fun getServerDrivenContainerId(): Int = R.id.server_driven_container
+
+    override fun getToolbar(): Toolbar = mToolbar
+
+    override fun onServerDrivenContainerStateChanged(state: ServerDrivenState) {
+        when (state) {
+            is ServerDrivenState.Started -> {
+                progressBar.visibility =  View.VISIBLE
+            }
+            is ServerDrivenState.Finished -> {
+                progressBar.visibility =  View.GONE
+            }
+            is ServerDrivenState.Error -> {
+                Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
